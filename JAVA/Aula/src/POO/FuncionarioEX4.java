79b9ab83401e0284ec00c61e30662860eee6ef71
Daniel <69875036+Danieloliver11@users.noreply.github.com>
2021-03-02 19:44:53 -0300

    POO

--- JAVA/Aula/src/POO/FuncionarioEX4.java
+++ JAVA/Aula/src/POO/FuncionarioEX4.java
@@ -1,13 +1,22 @@
-package POO;
 /*
 4) Crie uma classe funcionário e apresente os atributos e métodos
 referentes esta classe, em seguida crie um objeto funcionário, defina as
 instancias deste objeto e apresente as informações deste objeto no
 console.*/
+package POO;
+
 public class FuncionarioEX4 {
 
 	public static void main(String[] args) {
+		Funcionario fulano = new Funcionario();
+		
+		fulano.setNome("João das Neves");
+		fulano.setIdade(1991);
+		fulano.setDepartamento("Desenvolvedor");
+		
+		System.out.printf("O funcionário  %s do ID: %d idade: %d anos. Funcionario do departamento: %s",fulano.getNome(),fulano.getId(),fulano.getIdade(),fulano.getDepartamento());
 
+		
 	}
 
 }
